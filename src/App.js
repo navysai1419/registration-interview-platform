2a2a0c03178006346cd022e6888de31519ab125b
Navya <navyadummu@gmail.com>
2025-10-13 14:02:58 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RegistrationPage from './RegistrationPage'; // We'll create this next
+
+function App() {
+  return (
+    <div className="App">
+      <RegistrationPage />
+    </div>
+  );
+}
+
+export default App;
